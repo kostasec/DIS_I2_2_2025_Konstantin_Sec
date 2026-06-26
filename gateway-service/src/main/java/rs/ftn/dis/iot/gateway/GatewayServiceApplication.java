9e1ba955c1ad3e8ebd0bf88da67efa3384e9778a
@kostasec <kosta.sec99@gmail.com>
2026-06-26 21:20:03 +0200

    Add Spring Cloud Gateway and fix server ports

---
+++ gateway-service/src/main/java/rs/ftn/dis/iot/gateway/GatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package rs.ftn.dis.iot.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
